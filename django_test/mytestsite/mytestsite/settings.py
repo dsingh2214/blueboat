5d8935157bc413afecb4d151746c8cb39c5bc169
dsingh2214 <devangsingh2214@gmail.com>
2024-11-22 21:43:23 +0530

    static path

--- django_test/mytestsite/mytestsite/settings.py
+++ django_test/mytestsite/mytestsite/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'mytestsite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
@@ -121,7 +121,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, 'static')
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
